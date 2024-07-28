33b575d82a2c7f593865d992db264c19675e001c
dhananjeyan23 <jeyan.mech7@gmail.com>
2024-07-28 14:42:19 +0530

    opp

---
+++ src/org/demo/Emply.java
@@ -0,0 +1,17 @@
+package org.demo;
+
+public class Emply {
+	String ename1="padma";
+	//create the 1st method empName
+	private void empName1() {
+	System.out.println("employeename : "+ename1);
+	}
+	//create the main method 
+	public static void main(String[] args) {
+	//classname objrefname = new classname()
+		Emply ei1 = new Emply();
+		ei1.empName1();
+	
+
+}
+}
